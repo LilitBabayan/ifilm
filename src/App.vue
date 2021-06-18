9d641322a02947885d8be5038b6650afe675664b
Shushan <kharatyanshushan@gmail.com>
2021-06-18 15:41:12 +0400

    Footer component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/"></router-link>
-    </div>
-    <router-view/>
+    <MainFooter />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import MainFooter from '@/components/MainFooter'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  export default {
+    name: 'App',
+    components: {
+      MainFooter
     }
   }
-}
+</script>
+
+<style lang="scss">
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+  }
 </style>
